14eb9ca3237e28e9af9080c02ff91eec27180af7
zhanglin268 <62052466+zhanglin268@users.noreply.github.com>
2020-03-29 20:00:31 +0800

    Add files via upload

---
+++ test_3_14/test_3_14/test.c
@@ -0,0 +1,106 @@
+//#include<stdio.h>
+//#include<string.h>
+//int main()
+//{
+	//char arr[] = { 'b', 'i', 't' };
+	//printf("%d\n", strlen(arr));
+
+	//printf("%d\n", strlen("c:\test\121"));
+	//return 0;
+//}
+//#include<stdio.h>
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//scanf_s("%d%d", &a, &b);
+//	if (a>b)
+//		printf("%d\n", a);
+//	else
+//		printf("%d\n", b);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	printf("朱馨雨，认识你是我最大的幸福\n");
+//	printf("如果你愿意跟我一直走下去  \n");
+//	printf("我不会让你输\n");
+//	printf(" 520 13 14 \n");
+//	return 0;
+//}
+
+#include<stdio.h>
+
+//#define max 199
+//int main()
+//{
+	//const int num = 10;
+	
+	//printf("%d\n", num);
+	//int arr[10] = { '1','2','3','2','2','5','4','6','7','8'};
+	//int a = max;
+	/*char arr1[]="abc";
+	//char arr2[] = { 'a', 'b', 'c' };
+	//printf("%s\n", arr1);*/
+	//printf("\a\a\a\a\a");
+	//printf("\34\n");;
+
+
+	//kprintf("今天又是美好的一天 ！");
+	/*int n = 0;
+	//scanf_s("%d", &n);
+	//while (n > 90)
+	//{
+	//	printf("你的成绩优秀\n");
+	//}
+	//printf("你还得继续加油哦！\n");*/
+	//int num1 = 39;
+	//int num2 = 67;
+	//int sum = 0;
+	//sum = num1 + num2;
+	//printf("%d\n", sum);
+	
+	//int num2 = 0;
+	//int sum = 0;*/
+	//Add(int x,int y)
+	//scanf_s("%d%d", &num1, &num2);
+	////sum = num1 + num2;
+	//printf("%d\n", sum);
+
+
+
+//int Add(int x, int y)
+//{
+//	int z = x + y;
+//	return z;
+//}
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int sum = 0;
+//	scanf_s("%d%d", &a, &b);
+//	sum = Add(a ,b);
+//	printf("sum=%d\n", sum);
+//	return 0;
+//	
+//}
+
+
+int main()
+{
+	int arr[] = { 2, 3, 3, 3, 0,4,5,2,6,2,6,7,6,3,7,8,5 };
+	int i = 0;
+	while (i < 17)
+	{
+
+		
+		printf("%d\n", arr[i]);
+		i++;
+	}
+
+
+	return 0;
+}
